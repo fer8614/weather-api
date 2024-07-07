10fe0dd49cea7956eeb1a73ada109b8f0c1b9b29
fer8614 <fernando.inversionmf@gmail.com>
2024-07-06 17:48:29 -0700

    Validating the form

--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { countries } from "../data/countries";
 import styles from './Form.module.css'
 import { SearchType } from "../types";
+import Alert from "../components/Alert/Alert";
 
 export default function Form() {
 
@@ -10,6 +11,8 @@ export default function Form() {
         country: ''
     })
 
+    const [ alert, setAlert ] = useState('')
+
     const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         setSearch({
             ...search,
@@ -17,8 +20,21 @@ export default function Form() {
         })
     }
 
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if ( Object.values(search).includes('') ) {
+            setAlert('All fields are required')
+            return
+        }
+    }
+
   return (
-    <form className={styles.form}>
+    <form 
+        className={styles.form}
+        onSubmit={ handleSubmit }
+    >
+        { alert && <Alert>{alert}</Alert> }
         <div className={styles.field}>
             <label htmlFor="city">City: </label>
             <input 
